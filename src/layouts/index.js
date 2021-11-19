03944665adb52bf18764b6d4ed0947d6aa8ac631
Marcin Lasak <lasakmarcin90@gmail.com>
2021-11-19 17:07:02 +0100

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,6 +14,7 @@ const TemplateWrapper = ({ children }) => (
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
+        { name: 'google-site-verification' content: 'Q3AorUrT8RkIuqe3M9z5pNi4gWsO5VCRxJUzhWeklOc' },
       ]}
     ></Helmet>
     <Header />
